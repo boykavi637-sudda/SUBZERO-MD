7f4f91b51103d2cc6b79c5b34b1bf905b65fdd9c
boykavi637-sudda <boykavi637@gmail.com>
2025-10-02 16:49:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;PE81xQaD#h7mOAio9k-6P4tqv8i802mUw3H2NFexlIXbc452IEY4", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
